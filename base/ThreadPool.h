2961ecdcd6d8445bd9c7a8c9894e2952142d1849
Liluoquan <a853829801@qq.com>
2022-06-16 21:16:59 +0800

    finish threadPool

--- base/ThreadPool.h
+++ base/ThreadPool.h
@@ -13,6 +13,7 @@
 class ThreadPool {
 public:
     ThreadPool(size_t);
+    ~ThreadPool();
     template<typename F, typename... Args>
     auto enqueue(F&& f, Args&&... args)
         -> std::future<typename std::result_of<F(Args...)>::type>; // result_of 的作用与 decltype 相同，不国针对的是函数返回值
@@ -46,3 +47,45 @@ inline ThreadPool::ThreadPool(size_t threads) : _stop(false) {
         });
     }
 }
+
+template<typename F, typename... Args>
+auto ThreadPool::enqueue(F&& f, Args&&... args) 
+    -> std::future<typename std::result_of<F(Args...)>::type>
+{
+    using return_type = typename std::result_of<F(Args...)>::type;
+    
+    auto task = std::make_shared<std::packaged_task<return_type()>> (
+        std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    );
+
+    std::future<return_type> res = task->get_future();
+    {
+        std::unique_lock<std::mutex> lock(_queueMutex);
+
+        if(_stop)
+            throw std::runtime_error("enqueue on stopped ThreadPool");
+
+        _tasks.emplace([task]() {
+            (*task)();
+        });
+    }
+
+    _condition.notify_all();
+    return res;
+}
+
+
+inline ThreadPool::~ThreadPool() {
+    {
+        std::unique_lock<std::mutex> lock(_queueMutex);
+        _stop = true;
+    }
+    _condition.notify_all();
+    for(std::thread& worker : _workers) {
+        worker.join();
+    }
+}
+
+
+
+
